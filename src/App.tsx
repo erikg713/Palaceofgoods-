4c7cca7347742a07551af21267f2b20b303a054c
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-01-28 09:51:07 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,21 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import AdminPanel from "./pages/AdminPanel";
+
+const App: React.FC = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/admin" element={<AdminPanel />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
+
 import React from 'react';
 
 const App: React.FC = () => {
