c81b600cf407a4b5a08cb89b4d39b201ce587e6c
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-01-28 17:58:41 -0600

    Update api.ts

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,1 +1,22 @@
+import axios from 'axios';
 
+const api = axios.create({
+  baseURL: '/api', // This will be proxied to http://localhost:3001/api
+});
+
+export const login = async (email: string, password: string) => {
+  const response = await api.post('/login', { email, password });
+  return response.data;
+};
+
+export const getProducts = async () => {
+  const response = await api.get('/products');
+  return response.data;
+};
+
+export const submitReview = async (productId: string, rating: number, comment: string) => {
+  const response = await api.post('/reviews', { productId, rating, comment });
+  return response.data;
+};
+
+export default api;
