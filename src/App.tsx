8bd83a1dcef290b8b889a03dda9c3ea5238ada29
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-01-28 10:00:49 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,42 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import AdminPanel from "./pages/AdminPanel";
+import SellerDashboard from "./pages/SellerDashboard";
+import BuyerDashboard from "./pages/BuyerDashboard";
+import ProtectedRoute from "./utils/ProtectedRoute";
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="/admin" element={<AdminPanel />} />
+        <Route
+          path="/admin"
+          element={
+            <ProtectedRoute role="admin">
+              <AdminPanel />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/seller"
+          element={
+            <ProtectedRoute role="seller">
+              <SellerDashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/buyer"
+          element={
+            <ProtectedRoute role="buyer">
+              <BuyerDashboard />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </Router>
   );
-};
-
-export default App;
-
-import React from 'react';
-
-const App: React.FC = () => {
-  return <h1>Welcome to Palace of Goods!</h1>;
 };
 
 export default App;
