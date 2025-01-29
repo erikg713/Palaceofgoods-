9086c24ec4f99327f527c5b564ba672f78d51f74
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-01-28 18:13:17 -0600

    Update api.ts

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -15,3 +15,15 @@ export const getProducts = async () => {
 };
 
 export default api;
+export interface Product {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  sellerId: string;
+}
+export interface LoginResponse {
+  token: string;
+  user: string;
+  role: 'buyer' | 'seller' | 'admin';
+}
