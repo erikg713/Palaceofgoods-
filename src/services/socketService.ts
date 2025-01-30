577e20a185418bd570eea2d57dec7f6021a1aa23
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-01-30 10:24:04 -0600

    Update socketService.ts

--- src/services/socketService.ts
+++ src/services/socketService.ts
@@ -15,3 +15,25 @@ export const onReceiveMessage = (callback: (data: { sender: string; message: str
 };
 
 export default socket;
+import { io } from "socket.io-client";
+import useAuthStore from "../state/authStore";
+
+const token = useAuthStore.getState().token;
+
+const socket = io("http://localhost:3001", {
+  auth: { token }, // Send JWT Token
+});
+
+export const joinRoom = (room: string) => {
+  socket.emit("joinRoom", { room });
+};
+
+export const sendMessage = (room: string, message: string, sender: string) => {
+  socket.emit("sendMessage", { room, message, sender });
+};
+
+export const onReceiveMessage = (callback: (data: { sender: string; message: string }) => void) => {
+  socket.on("receiveMessage", callback);
+};
+
+export default socket;
