1a29740c443a8785135c5c4dd3595293d4ae104c
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-01-31 13:26:55 -0600

    Update piNetwork.ts

--- src/utils/piNetwork.ts
+++ src/utils/piNetwork.ts
@@ -5,7 +5,7 @@ import { Pi } from 'pi-sdk';
 export const initializePiNetwork = () => {
   const pi = new Pi({
     version: 'v2',
-    sandbox: process.env.NODE_ENV !== 'production',
+    sandbox: process.env.NODE_ENV !== 'development,
   });
 
   return pi;
