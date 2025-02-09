9e705ae8fa49abd530765ec7434bcde189c1a1eb
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-02-09 01:45:41 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,34 @@
-import React from 'react';  
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';  
-import Home from './pages/Home';  
-import About from './pages/About';  
-import ProductList from './pages/ProductList';  
-import ProductDetail from './pages/ProductDetail';  
-import Header from './components/Header';  
-import Footer from './components/Footer';  
-import React from 'react';  
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';  
-import Home from './pages/Home';  
-import Login from './pages/Login';  
-import Register from './pages/Register';  
-import Profile from './pages/Profile';  
-import ProductList from './pages/ProductList';  
-import ProductDetail from './pages/ProductDetail';  
-import ShoppingCart from './components/ShoppingCart';  
-import Header from './components/Header';  
-import Footer from './components/Footer';  
-import { StoreProvider } from './state/store';  
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Profile from './pages/Profile';
+import ProductList from './pages/ProductList';
+import ProductDetail from './pages/ProductDetail';
+import ShoppingCart from './components/ShoppingCart';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { useStore } from './state/store'; // Import Zustand store
 
-const App: React.FC = () => {  
-  return (  
-    <StoreProvider>  
-      <Router>  
-        <Header />  
-        <Routes>  
-          <Route path="/" element={<Home />} />  
-          <Route path="/login" element={<Login />} />  
-          <Route path="/register" element={<Register />} />  
-          <Route path="/profile" element={<Profile />} />  
-          <Route path="/products" element={<ProductList />} />  
-          <Route path="/products/:id" element={<ProductDetail />} />  
-          <Route path="/cart" element={<ShoppingCart />} />  
-        </Routes>  
-        <Footer />  
-      </Router>  
-    </StoreProvider>  
-  );  
-};  
+const App: React.FC = () => {
+  const { user } = useStore(); // Access user from Zustand store
 
-export default App;const App: React.FC = () => {  
-  return (  
-    <Router>  
-      <Header />  
-      <Routes>  
-        <Route path="/" element={<Home />} />  
-        <Route path="/about" element={<About />} />  
-        <Route path="/products" element={<ProductList />} />  
-        <Route path="/products/:id" element={<ProductDetail />} />  
-      </Routes>  
-      <Footer />  
-    </Router>  
-  );  
-};  
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/profile" element={user ? <Profile /> : <Navigate to="/login" />} /> {/* Protected route */}
+        <Route path="/products" element={<ProductList />} />
+        <Route path="/products/:id" element={<ProductDetail />} />
+        <Route path="/cart" element={<ShoppingCart />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+};
 
 export default App;
