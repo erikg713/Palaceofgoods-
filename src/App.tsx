85864370eec0010350600e45e80616e2ed50cece
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-02-09 00:09:24 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,25 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./components/Login";
-import AdminPanel from "./pages/AdminPanel";
-import SellerDashboard from "./pages/SellerDashboard";
-import BuyerDashboard from "./pages/BuyerDashboard";
-import ProtectedRoute from "./utils/ProtectedRoute";
+import React from 'react';  
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';  
+import Home from './pages/Home';  
+import About from './pages/About';  
+import ProductList from './pages/ProductList';  
+import ProductDetail from './pages/ProductDetail';  
+import Header from './components/Header';  
+import Footer from './components/Footer';  
 
-const App: React.FC = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route
-          path="/admin"
-          element={
-            <ProtectedRoute role="admin">
-              <AdminPanel />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/seller"
-          element={
-            <ProtectedRoute role="seller">
-              <SellerDashboard />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/buyer"
-          element={
-            <ProtectedRoute role="buyer">
-              <BuyerDashboard />
-            </ProtectedRoute>
-          }
-        />
-      </Routes>
-    </Router>
-  );
-};
+const App: React.FC = () => {  
+  return (  
+    <Router>  
+      <Header />  
+      <Routes>  
+        <Route path="/" element={<Home />} />  
+        <Route path="/about" element={<About />} />  
+        <Route path="/products" element={<ProductList />} />  
+        <Route path="/products/:id" element={<ProductDetail />} />  
+      </Routes>  
+      <Footer />  
+    </Router>  
+  );  
+};  
 
 export default App;
