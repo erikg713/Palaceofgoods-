65cb080490f0e8657f7ee631151b4982cc4d3cb5
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-02-12 00:17:16 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
-    proxy: {
-      '/api': 'http://localhost:3001', // Proxy API requests to backend
-    },
+    port: 5173, // Change if needed
   },
 });
